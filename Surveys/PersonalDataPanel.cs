2b74c9529b585299c295f0f318f43ae5541c839c
Leoltron <leoole5@gmail.com>
2017-07-01 12:41:30 +0500

    Added PersonalDataPanel to SufveyForm

---
+++ Surveys/PersonalDataPanel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Surveys
+{
+    class PersonalDataPanel : Panel
+    {
+        private Label[] Names;
+        private TextBox[] Values;
+
+        private const int ElementHeight = 20;
+        private const int ElementMargin = 20;
+
+        public PersonalDataPanel(string[] dataPoints)
+        {
+            Names = new Label[dataPoints.Length];
+            Values = new TextBox[dataPoints.Length];
+            for (var i = 0; i < Names.Length; i++)
+            {
+                Names[i] = new Label
+                {
+                    Text = dataPoints[i],
+                    Location = new Point(ElementMargin, (i + 1) * ElementMargin + i * ElementHeight),
+                };
+                Names[i].Size =
+                    new Size(
+                        TextRenderer.MeasureText(
+                            dataPoints[i],
+                            Names[i].Font,
+                            new Size(int.MaxValue, int.MaxValue)
+                        ).Width,
+                        ElementHeight);
+
+                Controls.Add(Names[i]);
+
+                Values[i] = new TextBox {Location = new Point(Names[i].Right + ElementMargin, Names[i].Top - 2)};
+                Controls.Add(Values[i]);
+            }
+
+            Resize += (sender, args) => OnResize();
+            OnResize();
+        }
+
+        private void OnResize()
+        {
+            for (var i = 0; i < Names.Length; i++)
+                Values[i].Size = new Size(Width - Names[i].Right - 100, ElementHeight);
+        }
+
+        public Tuple<string, string>[] GetPersonalData()
+        {
+            var result = new Tuple<string, string>[Names.Length];
+            for (var i = 0; i < result.Length; i++)
+                result[i] = Tuple.Create(Names[i].Text, Values[i].Text);
+            return result;
+        }
+    }
+}
--- Surveys/SurveyForm.cs
+++ Surveys/SurveyForm.cs
@@ -1,24 +1,98 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Surveys
 {
     sealed class SurveyForm : Form
     {
-        private Survey survey;
+        private readonly Survey survey;
 
-        private Button nextButton;
-        private Button prevButton;
+        private readonly Button nextButton;
+        private readonly Button prevButton;
+        private readonly Label title;
+        private Panel[] panels;
+
+        private int currentPage;
+        private int PagesCount => survey.QuestionsAmount + (HavePersonalDataPoints ? 1 : 0);
+
+        private bool HavePersonalDataPoints => survey.PersonalDataPoints.Length > 0;
+
+        private bool IsLastPage => currentPage == PagesCount - 1;
+        private bool IsPersonalDataPage => currentPage == 0 && HavePersonalDataPoints;
+        private int GetCurrentQuestionNumber => HavePersonalDataPoints ? currentPage - 1 : currentPage;
 
         public SurveyForm(Survey survey)
         {
             Text = @"Опрос";
             Icon = new Icon("icon.ico");
+            MinimumSize = new Size(500, 400);
+
             this.survey = survey;
 
+            panels = new Panel[PagesCount];
+
+            if (HavePersonalDataPoints)
+                panels[0] = new PersonalDataPanel(survey.PersonalDataPoints) {Visible = true};
+
+            var i = HavePersonalDataPoints ? 1 : 0;
+            foreach (var question in survey.Questions)
+            {
+                panels[i] = new Panel() {Visible = false};
+                i++;
+            }
+
+            foreach (var panel in panels)
+            {
+                panel.BorderStyle = BorderStyle.FixedSingle;
+                panel.AutoScroll = true;
+                panel.Location = new Point(0,50);
+                Controls.Add(panel);
+            }
+
+
+            title = new Label
+            {
+                Location = new Point(10, 10),
+                Font = new Font(Font.FontFamily.Name, 18f),
+                Size = new Size(500, 40)
+            };
+            Controls.Add(title);
+
             nextButton = new Button();
-            prevButton = new Button();
+            Controls.Add(nextButton);
+
+            prevButton = new Button {Text = @"Назад"};
+            Controls.Add(prevButton);
+
+            UpdateLabels();
+            Resize += (sender, args) => OnResize();
+            Load += (sender, args) => OnResize();
         }
-        
+
+        private void UpdateLabels()
+        {
+            title.Text = IsPersonalDataPage
+                ? @"Пожалуйста, укажите свои данные"
+                : $"Вопрос №{GetCurrentQuestionNumber + 1} из {survey.Questions.Length}";
+            nextButton.Text = IsLastPage ? @"Готово" : @"Далее";
+            prevButton.Enabled = currentPage > 0;
+        }
+
+        private const int ButtonMargin = 5;
+        private const int ButtonWidth = 100;
+        private const int ButtonHeight = 30;
+
+        private void OnResize()
+        {
+            prevButton.Location = new Point(ButtonMargin, Height - ButtonMargin - ButtonHeight - 40);
+            nextButton.Location = new Point(Width - ButtonMargin - ButtonWidth - 16,
+                Height - ButtonMargin - ButtonHeight - 40);
+
+            nextButton.Size = prevButton.Size = new Size(ButtonWidth, ButtonHeight);
+
+            foreach (var panel in panels)
+                panel.Size = new Size(Width-16, Height - nextButton.Height - 100);
+        }
     }
 }
